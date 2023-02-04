f88f4bfc6f1b30150390ab7183265a234ec6ff49
acmarchiori <ac_marchiori@yahoo.com.br>
2023-02-03 21:01:04 -0300

    Finalizado requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,88 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
+
+const INITIAL_STATE = {
+  nameInput: '',
+  buttonEnable: true,
+  isLoading: false,
+};
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = INITIAL_STATE;
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    }, this.btnDisable);
+  };
+
+  handleClick = () => {
+    const { history } = this.props;
+    const { nameInput } = this.state;
+    this.setState({
+      isLoading: true,
+    }, async () => {
+      await createUser({ name: nameInput });
+      history.push('/search');
+    });
+  };
+
+  btnDisable = () => {
+    const { nameInput } = this.state;
+    const minCharacter = 3;
+    if (nameInput.length >= minCharacter) {
+      this.setState({ buttonEnable: false });
+    }
+  };
+
   render() {
+    const {
+      nameInput,
+      buttonEnable,
+      isLoading,
+    } = this.state;
+
     return (
       <div data-testid="page-login">
-        <p>Login</p>
+        {
+          isLoading ? <Loading /> : (
+            <form>
+              <input
+                data-testid="login-name-input"
+                type="text"
+                name="nameInput"
+                id="nameInput"
+                required
+                value={ nameInput }
+                onChange={ this.handleChange }
+              />
+              <button
+                data-testid="login-submit-button"
+                disabled={ buttonEnable }
+                onClick={ this.handleClick }
+              >
+                Entrar
+              </button>
+            </form>
+          )
+        }
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.string,
+  }),
+}.isrequired;
 
 export default Login;
