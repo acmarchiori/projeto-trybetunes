76aa21c03d15a84bf5358fd92822d71b2d7b4c87
acmarchiori <ac_marchiori@yahoo.com.br>
2023-02-06 21:16:14 -0300

    Refatorado requisito 7

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import getMusics from '../services/musicsAPI';
-import { addSong } from '../services/favoriteSongsAPI';
+// import { addSong } from '../services/favoriteSongsAPI';
 // import Loading from '../pages/Loading';
 
 class MusicCard extends Component {
   state = {
-    artistName: '',
-    collectionName: '',
-    artworkUrl100: '',
-    generatedList: [],
     // checked: false,
-    trackId: '',
+    // trackId: '',
   };
 
-  componentDidMount() {
-    this.fetchMusic();
-    this.fetchFavorite();
-  }
+  // componentDidMount() {
+  //   this.fetchMusic();
+  //   this.fetchFavorite();
+  // }
 
   handleChange = ({ target }) => {
     const { name, type } = target;
@@ -25,79 +20,56 @@ class MusicCard extends Component {
     this.setState({
       [name]: newValue,
     });
-    // console.log(target.id);
-    this.setState({ trackId: target.id });
   };
 
-  fetchFavorite = async () => {
-    const { trackId } = this.state;
-    const favorite = await addSong(trackId);
-    return favorite;
-  };
-
-  fetchMusic = async () => {
-    const { id } = this.props;
-    const list = await getMusics(id);
-    // console.log(list);
-    this.setState({
-      artistName: list[0].artistName,
-      collectionName: list[0].collectionName,
-      artworkUrl100: list[0].artworkUrl100,
-      generatedList: list,
-    });
-  };
+  // fetchFavorite = async () => {
+  //   const { trackId } = this.state;
+  //   const favorite = await addSong(trackId);
+  //   return favorite;
+  // };
 
   list = () => {
-    const { generatedList } = this.state;
-    // console.log(generatedList);
+    const { musics } = this.props;
     return (
-      generatedList.slice(1).map((musics) => (
-        <div key={ musics.trackNumber }>
-          <li>
-            <p>{ musics.trackName }</p>
-            <audio
-              data-testid="audio-component"
-              src={ musics.previewUrl }
-              controls
-            >
-              <track kind="captions" />
-              O seu navegador não suporta o elemento
-              {' '}
-              {' '}
-              <code>audio</code>
-              .
-            </audio>
-            <label htmlFor="favorita">
-              Favorita
-              <input
-                data-testid={ `checkbox-music-${musics.trackId}` }
-                type="checkbox"
-                name="favorita"
-                id={ musics }
-                // checked={ checked }
-                onChange={ this.handleChange }
-              />
-            </label>
-          </li>
-        </div>
-      ))
+      <div>
+        <li>
+          <p>{ musics.trackName }</p>
+          <audio
+            data-testid="audio-component"
+            src={ musics.previewUrl }
+            controls
+          >
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            {' '}
+            {' '}
+            <code>audio</code>
+            .
+          </audio>
+          <label htmlFor="favorita">
+            Favorita
+            <input
+              data-testid={ `checkbox-music-${musics.trackId}` }
+              type="checkbox"
+              name="favorita"
+              id={ musics }
+              // checked={ checked }
+              onChange={ this.handleChange }
+            />
+          </label>
+        </li>
+      </div>
     );
   };
 
   render() {
-    const { artistName, collectionName, artworkUrl100 } = this.state;
     return (
       <div>
-        <img src={ artworkUrl100 } alt={ collectionName } />
-        <h2 data-testid="album-name">{ collectionName }</h2>
-        <h2 data-testid="artist-name">{ artistName }</h2>
-        <div>
-          <ol>
-            {
-              this.list()
-            }
-          </ol>
-        </div>
+        <ol>
+          {
+            this.list()
+          }
+        </ol>
       </div>
     );
   }
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -2,14 +2,56 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MusicCard from '../components/MusicCard';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  state = {
+    artistName: '',
+    collectionName: '',
+    artworkUrl100: '',
+    generatedList: [],
+  };
+
+  componentDidMount() {
+    this.fetchMusic();
+  }
+
+  fetchMusic = async () => {
+    const { match: { params: { id } } } = this.props;
+    const list = await getMusics(id);
+    // console.log(list[0].artworkUrl100);
+    this.setState({
+      artistName: list[0].artistName,
+      collectionName: list[0].collectionName,
+      artworkUrl100: list[0].artworkUrl100,
+      generatedList: list,
+    });
+  };
+
   render() {
-    const { match } = this.props;
+    const {
+      artistName,
+      collectionName,
+      artworkUrl100,
+      generatedList,
+    } = this.state;
+
     return (
       <div data-testid="page-album">
         <Header />
-        <MusicCard id={ match.params.id } />
+        <section>
+          <img src={ artworkUrl100 } alt={ collectionName } />
+          <h2 data-testid="album-name">{ collectionName }</h2>
+          <h2 data-testid="artist-name">{ artistName }</h2>
+        </section>
+        <div>
+          {
+            generatedList.slice(1).map((music) => (<MusicCard
+              key={ music.trackNumber }
+              musics={ music }
+            />))
+          }
+        </div>
       </div>
     );
   }
@@ -17,8 +59,10 @@ class Album extends Component {
 
 Album.propTypes = {
   match: PropTypes.shape({
-    params: PropTypes.string,
-  }),
-}.isrequired;
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Album;
